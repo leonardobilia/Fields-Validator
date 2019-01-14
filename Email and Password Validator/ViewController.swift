bc6ae7782764b64bdffcdb6b87b9e974af1bfdaa
Leonardo Bilia <leonardo.bilia@gmail.com>
2019-01-14 10:48:36 -0200

    Initial Commit

---
+++ Email and Password Validator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Email and Password Validator
+//
+//  Created by Leonardo Bilia on 1/14/19.
+//  Copyright © 2019 Leonardo Bilia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
